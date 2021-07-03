520cdfea4cd305f22e9c1247e98708bef262bfc8
Scape <wuzhuangtai00@foxmail.com>
2021-07-03 16:33:25 +0800

    brute_force debug

--- brute_force.cpp
+++ brute_force.cpp
@@ -4,6 +4,7 @@
 #include<algorithm>
 #include<vector>
 #include<set>
+#include<cmath>
 #define rep(i, a, b) for(int i = (a); i <= (b); i++)
 #define per(i, a, b) for(int i = (a); i >= (b); i--)
 using namespace std;
