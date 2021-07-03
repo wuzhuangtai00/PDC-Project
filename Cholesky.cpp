77e52c41da78f7a5250666de3832afb6ad4a1578
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 04:59:38 +0800

    try

--- Cholesky.cpp
+++ Cholesky.cpp
@@ -21,7 +21,7 @@ typedef pair<int,int> pin;
 const double eps = 1e-3;
 typedef pair<int, double> info;
 const int maxm = 2000000000;
-info pool1[5000000000ll];
+info pool1[6000000000ll];
 info *ptr;
 set<pin> rec;
 
@@ -100,7 +100,7 @@ long long cnt = 0;
 
 inline info* mlc(int sz) {
     cnt += sz;
-    if(cnt > 5000000000ll) {
+    if(cnt > 6000000000ll) {
         // ptr = pool2;
         assert(0);
     }
