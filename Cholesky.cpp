bfbd91dc0aafde05d76b599ed6a52a3e6d6391a2
Scape <wuzhuangtai00@foxmail.com>
2021-07-03 22:54:09 +0800

    Cholesky

--- Cholesky.cpp
+++ Cholesky.cpp
@@ -2,10 +2,19 @@
 #include<cstdio>
 #include<cstdlib>
 #include<algorithm>
+#include<ctime>
+#include<vector>
+#include<set>
+#include<cmath>
 #define rep(i, a, b) for(int i = (a); i <= (b); i++)
 #define per(i, a, b) for(int i = (a); i >= (b); i--)
 using namespace std;
+#define pb push_back
+#define mk make_pair
+#define w1 first
+#define w2 second
 
+typedef pair<int,int> pin;
 
 inline void read(int &x){
     char ch = getchar();
@@ -13,7 +22,7 @@ inline void read(int &x){
         while(ch != '\n') ch = getchar();
         ch = getchar();
     }
-	x=0;int f=1;char ch=getchar();
+	x=0;int f=1;
 	while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
 	while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
 	x*=f;
@@ -30,9 +39,24 @@ inline void init() {
     }
 }
 
+
 int main() {
+	clock_t pre = clock();
+
     init();
+
+	printf("Init: %.4f\n", (clock() - pre)/(double)CLOCKS_PER_SEC);
+	pre = clock();
 
     solve();
 
+	printf("Solve: %.4f\n", (clock() - pre)/(double)CLOCKS_PER_SEC);
+	pre = clock();
+
+	check_ans();
+
+	printf("Check Ans: %.4f\n", (clock() - pre)/(double)CLOCKS_PER_SEC);
+	pre = clock();
+
+	// output();
 }
