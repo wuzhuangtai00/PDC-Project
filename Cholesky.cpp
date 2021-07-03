fea38ae082d8ec8e90a5bb7f55df5d249c7bcd3a
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 04:42:18 +0800

    try

--- Cholesky.cpp
+++ Cholesky.cpp
@@ -20,7 +20,7 @@ typedef pair<int,int> pin;
 
 const double eps = 1e-3;
 typedef pair<int, double> info;
-const long long maxm = 10000000000;
+const int maxm = 2000000000;
 info pool[maxm];
 info *ptr;
 set<pin> rec;
