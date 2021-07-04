f0e2d724c74c7452daf2a313ecad47dd4be44707
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 08:37:41 +0800

    check

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-g++ normal.cpp -o normal -O2 -mcmodel=medium -std=c++11
+g++ normal.cpp -o normal -O2 -mcmodel=medium -std=c++11 -fopenmp
 ./normal 1138_bus.mtx l.txt u.txt 
 ./check 1138_bus.mtx l.txt u.txt
 ./normal bcsstk18.mtx l.txt u.txt
