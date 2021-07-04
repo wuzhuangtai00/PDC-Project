f16e4f63075c4643f2806430e13475985e3cd884
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 19:13:54 +0800

    try

---
+++ mac.sh
@@ -0,0 +1,5 @@
+g++ faq.cpp -o problem_3 -mcmodel=medium -I /usr/local/opt/libomp/include/ -L /usr/local/opt/libomp/lib -lomp -O2 -std=c++11
+./problem_3 1138_bus.mtx l1.txt u1.txt 
+# ./check 1138_bus.mtx l1.txt u1.txt
+./problem_3 bcsstk18.mtx l1.txt u1.txt
+# ./check bcsstk18.mtx l1.txt u1.txt
