78051152eeb547a9ff30b94a0b028c4d47b1a1c9
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 22:28:00 +0800

    try

--- faq.cpp
+++ faq.cpp
@@ -99,12 +99,12 @@ inline void solve() {
 		rep(p, 1, cnt) {
 			int j = cur[p];
 			if (fabs(a[i][j])>1e-3){
-				fprintf(u, "%d %d %.15lf\n", i, j, a[i][j]);
+				// fprintf(u, "%d %d %.15lf\n", i, j, a[i][j]);
 				nmsl[++cnm] = j;
 				val[cnm] = a[i][j];
 			}
 		}
-		fprintf(l, "%d %d %.12f\n", i, i, 1.0);
+		// fprintf(l, "%d %d %.12f\n", i, i, 1.0);
 		if(fabs(a[i][i]) < eps) continue;
 		if(a[i][i] == 0) continue;
 		int hr = 0;
@@ -113,7 +113,7 @@ inline void solve() {
 		rep(p, 1, hr) {
 			int k = par[p];
 			double d = a[k][i] / a[i][i];
-			fprintf(l, "%d %d %.15lf\n", k, i, d);
+			// fprintf(l, "%d %d %.15lf\n", k, i, d);
 			rep(t, 1, cnm){
 				int j = nmsl[t];
 				a[k][j] -= val[t] * d;
