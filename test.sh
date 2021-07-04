f2689fce878d9ca5c72faa3a44f7892f8e0a05ca
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 15:09:05 +0800

    cholesky

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 g++ normal.cpp -o normal -O2 -mcmodel=medium -std=c++11 -fopenmp
-./normal 1138_bus.mtx l.txt u.txt 
-./check 1138_bus.mtx l.txt u.txt
-./normal bcsstk18.mtx l.txt u.txt
-./check bcsstk18.mtx l.txt u.txt
+./normal 1138_bus.mtx l3.txt u3.txt 
+./check 1138_bus.mtx l3.txt u3.txt
+./normal bcsstk18.mtx l3.txt u3.txt
+./check bcsstk18.mtx l3.txt u3.txt
