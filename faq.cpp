9f8891f3ee6c27c5fdbc9b89b13522f98d155757
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 09:02:06 +0800

    check

--- faq.cpp
+++ faq.cpp
@@ -90,12 +90,17 @@ inline void solve() {
 			cur[++cnt] = x;
 			x = parent[x];
 		}int cnm = 0;
+		#pragma omp for
 		rep(p, 1, cnt) {
+#pragma task
+{
 			int j = cur[p];
 			if (fabs(a[i][j])>1e-7){
 				fprintf(u, "%d %d %.20lf\n", i, j, a[i][j]);
+				#pragma critical
 				nmsl[++cnm] = j;
 			}
+}
 		}
 		fprintf(l, "%d %d %.12f\n", i, i, 1.0);
 		if(fabs(a[i][i]) < eps) continue;
