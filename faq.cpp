9e4638fb57ce9a8001763b64727599a5883adae4
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 09:00:50 +0800

    check

--- faq.cpp
+++ faq.cpp
@@ -80,8 +80,10 @@ inline void solve() {
         }
     }
 
-#pragma omp parallel num_threads(12) {
-#pragma omp single{
+#pragma omp parallel num_threads(12) 
+{
+#pragma omp single
+{
 	rep(i, 1, n) {
 		int cnt = 0, x = i;
 		while(x) {
