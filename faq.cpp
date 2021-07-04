759877ead075082021bbbf58ecc69a2d663d3abc
Scape <wuzhuangtai00@foxmail.com>
2021-07-04 09:08:25 +0800

    check

--- faq.cpp
+++ faq.cpp
@@ -86,7 +86,7 @@ inline void solve() {
 			cur[++cnt] = x;
 			x = parent[x];
 		}int cnm = 0;
-#pragma omp parallel num_threads(12) for
+#pragma omp parallel for num_threads(12) 
 		rep(p, 1, cnt) {
 #pragma task
 {
